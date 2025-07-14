eacc1250eaa7423921c39d3bec550b30b6693a13
Dariofelix84 <dariofa69@gmail.com>
2025-07-14 17:34:13 -0300

    Create PedidoService

--- src/main/java/com/felix/microservicos/pedido/Service/PedidoService.java
+++ src/main/java/com/felix/microservicos/pedido/Service/PedidoService.java
@@ -1,7 +1,11 @@
 package com.felix.microservicos.pedido.Service;
 
+import com.felix.microservicos.pedido.Model.ItemPedido;
+import com.felix.microservicos.pedido.Model.Pedido;
 import com.felix.microservicos.pedido.Repository.PedidoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PedidoService {
@@ -10,5 +14,19 @@ public class PedidoService {
 
     public PedidoService(PedidoRepository pedidoRepository) {
         this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido salvarPedido(Pedido pedido) {
+
+        if (pedido.getItems() != null) {
+            for (ItemPedido item : pedido.getItems()) {
+                item.setPedido(pedido);
+            }
+        }
+       return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidoRepository.findAll();
     }
 }
